a8416cd071bb5efe5a3081276b7b757be25b617c
Kirill Lazarenko <kirill.lazarenko@gmail.com>
2020-06-25 13:41:26 +0300

    first commit

---
+++ precache-manifest.d9afc446d75c422675b2b238cc136de5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bcd05799570790bcdf25b72fe657926",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4306a2adbfee7d37488a",
+    "url": "/static/css/main.469b08c7.chunk.css"
+  },
+  {
+    "revision": "a7775846c5c2648ab044",
+    "url": "/static/js/2.06f470ea.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.06f470ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4306a2adbfee7d37488a",
+    "url": "/static/js/main.9c8a6b03.chunk.js"
+  },
+  {
+    "revision": "6fb4df4bc8d55d33537c",
+    "url": "/static/js/runtime-main.5c11ddc5.js"
+  },
+  {
+    "revision": "782adc2b6062ab00461359da5b02b753",
+    "url": "/static/media/defprofile.782adc2b.png"
+  }
+]);
